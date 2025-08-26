d88832b1689e429e369f23dacd5dd6bcb82f22b8
Mikhail Senchakov <mssenchakov@avito.ru>
2025-08-26 20:26:52 +0300

    autoReveal when isSearchVisible

--- src/modules/autoReveal.ts
+++ src/modules/autoReveal.ts
@@ -1,11 +1,17 @@
 import vscode from 'vscode';
 
+async function isSearchVisible(): Promise<boolean> {
+    return await vscode.commands.executeCommand('getContextKeyValue', 'searchViewletVisible') as boolean;
+}
+
 export const autoReveal = () => {
-    vscode.window.onDidChangeActiveTextEditor((editor) => {
+    vscode.window.onDidChangeActiveTextEditor(async (editor) => {
         const config = vscode.workspace.getConfiguration('quick-tools');
         const autoReveal = config.get('autoReveal', false);
 
-        if (editor && autoReveal) {
+        const searchVisible = await isSearchVisible();
+
+        if (editor && autoReveal && searchVisible) {
             vscode.commands.executeCommand('workbench.files.action.showActiveFileInExplorer');
         }
     });
